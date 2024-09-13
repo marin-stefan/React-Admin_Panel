63198f1eb4cbf54e63477d0efcbe2f7dae712c70
stefann06 <stefann06@yahoo.com>
2024-09-13 13:37:12 +0300

    replaced BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -3,22 +3,13 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Page404 from "./pages/Page404";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
-    {/* <Switch>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<Page404 />} />
-    </Switch> */}
-
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
