2bf10ed115b48255c38be5445e85cb8340a81232
stefann06 <stefann06@yahoo.com>
2024-09-13 12:49:47 +0300

    Updates

---
+++ precache-manifest.acc9184ae305157ba5a122806c79647d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0a867403205f88d4db6bcd2256280f",
+    "url": "/React-Admin_Panel/index.html"
+  },
+  {
+    "revision": "07663f93fac0442812f6",
+    "url": "/React-Admin_Panel/static/css/2.a22b3768.chunk.css"
+  },
+  {
+    "revision": "a77bb6f8f607a25b3d15",
+    "url": "/React-Admin_Panel/static/css/main.8ecb2532.chunk.css"
+  },
+  {
+    "revision": "07663f93fac0442812f6",
+    "url": "/React-Admin_Panel/static/js/2.ad1c8df7.chunk.js"
+  },
+  {
+    "revision": "a77bb6f8f607a25b3d15",
+    "url": "/React-Admin_Panel/static/js/main.e07313cd.chunk.js"
+  },
+  {
+    "revision": "4431609294d091a30922",
+    "url": "/React-Admin_Panel/static/js/runtime-main.ab7a3baf.js"
+  }
+]);
