43822b4633daa22e3917723bfcbb77ec017041d3
stefann06 <stefann06@yahoo.com>
2024-09-13 13:38:15 +0300

    Updates

--- precache-manifest.789fdfae3470d25dc654b53064115713.js
+++ precache-manifest.789fdfae3470d25dc654b53064115713.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08767bf4053c8597af8a06a1beb129b9",
+    "revision": "2b3426a93f5cf61c6fe7b04f4dc2faea",
     "url": "/React-Admin_Panel/index.html"
   },
   {
-    "revision": "658b9aac34d6d01560aa",
+    "revision": "8dd62fa49c2e9ed8f6f3",
     "url": "/React-Admin_Panel/static/css/2.cd1d3a8f.chunk.css"
   },
   {
-    "revision": "b1ac55a4729cc966ffc6",
+    "revision": "9fd4146d3ad9b62f5114",
     "url": "/React-Admin_Panel/static/css/main.8ecb2532.chunk.css"
   },
   {
-    "revision": "658b9aac34d6d01560aa",
-    "url": "/React-Admin_Panel/static/js/2.49c92a05.chunk.js"
+    "revision": "8dd62fa49c2e9ed8f6f3",
+    "url": "/React-Admin_Panel/static/js/2.345b1170.chunk.js"
   },
   {
-    "revision": "b1ac55a4729cc966ffc6",
-    "url": "/React-Admin_Panel/static/js/main.f4c4b0cb.chunk.js"
+    "revision": "9fd4146d3ad9b62f5114",
+    "url": "/React-Admin_Panel/static/js/main.0ab694a2.chunk.js"
   },
   {
     "revision": "4431609294d091a30922",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Admin_Panel/precache-manifest.9413791885b9577409ba37e35c24a1dd.js"
+  "/React-Admin_Panel/precache-manifest.789fdfae3470d25dc654b53064115713.js"
 );
 
 self.addEventListener('message', (event) => {
